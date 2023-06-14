4a132092ac07b6a388f8203d9417914f8cce5575
Sdanisjb <sandro.caceres.olave@gmail.com>
2023-06-14 14:39:48 -0500

    updated bd path

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from pusher import Pusher
 import sqlite3
+import os.path
 
 app = Flask(__name__)
 
@@ -15,7 +16,9 @@ pusher = Pusher(
 
 
 def get_db_connection():
-    conn = sqlite3.connect("database.db")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "database.db")
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
